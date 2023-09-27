00dc1c46468ce028fc5cdef4e3800360f7eae098
Gareth Gillman <gareth.gillman@brace.co.uk>
2023-09-27 15:42:15 +0100

    image row

--- acf/blocks/templates/image-row-block.php
+++ acf/blocks/templates/image-row-block.php
@@ -11,7 +11,7 @@ if( $images ):
     echo '<section class="image-row pt-8 lg:pt-14">';
         if( $title ):
             echo '<div class="title">';
-                echo '<div class="container mx-auto px-10 xl:px-0">';
+                echo '<div class="container mx-auto px-10 2xl:px-0">';
                     echo '<h2 class="mb-6 text-lg text-orange uppercase lg:mb-12 lg:text-3xl">'.$title.'</h2>';
                 echo '</div>';
             echo '</div>';
@@ -30,20 +30,20 @@ if( $images ):
                     endif;
 
                     if( $image ):
-                        echo '<div class="item md:relative basis-'.$image_width.'">';
+                        echo '<div class="item md:basis-2/4 md:relative basis-'.$image_width.'">';
 
                             if( !empty($link_url) ):
-                                echo '<a href="'.$link_url.'">';
+                                echo '<a class="block h-full w-full" href="'.$link_url.'">';
                             endif;
 
-                            echo wp_get_attachment_image($image, 'full');
+                            echo wp_get_attachment_image($image, 'full', '', array('class' => 'h-full') );
 
                             if( $hover_content ):
                                 $icon = $hover_content['icon'];
                                 $title = $hover_content['title'];
                                 $text = $hover_content['text'];
 
-                                echo '<div class="content bg-midblue p-6 text-center md:bg-midblue/80">';
+                                echo '<div class="content bg-midblue p-6 text-center md:absolute md:flex md:flex-col md:justify-center md:h-full md:w-full md:bg-midblue/90 md:left-0 md:top-0 xl:hidden">';
                                     if( $icon ):
                                         echo '<div class="icon text-center">';
                                             echo wp_get_attachment_image($icon, 'full', '', array('class' => 'h-[30px] mb-4 mx-auto w-[30px]'));
