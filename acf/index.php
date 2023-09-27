fc3b447fe72458d43c666d82f1c4bfaf758659e1
Gareth Gillman <gareth.gillman@brace.co.uk>
2023-09-27 13:22:58 +0100

    update acf blocks call

--- acf/index.php
+++ acf/index.php
@@ -2,8 +2,8 @@
 
 add_action( 'init', 'register_acf_blocks', 5 );
 function register_acf_blocks() {
-    register_block_type( dirname(__FILE__) . './blocks/column-content-block.json' );
-    register_block_type( dirname(__FILE__) . './blocks/slider-block.json');
-    register_block_type( dirname(__FILE__) . './blocks/hero-block.json');
-    register_block_type( dirname(__FILE__) . './blocks/contact-bar-block.json');
+    register_block_type( __DIR__ . '/blocks/column-content-block.json' );
+    register_block_type( __DIR__ . '/blocks/slider-block.json');
+    register_block_type( __DIR__ . '/blocks/hero-block.json');
+    register_block_type( __DIR__ . '/blocks/contact-bar-block.json');
 }
