ea0adae99e11c127118fc4991d496f004c4503e2
Gareth Gillman <gareth.gillman@brace.co.uk>
2023-09-27 10:04:14 +0100

    update blocks

--- acf/index.php
+++ acf/index.php
@@ -2,5 +2,5 @@
 
 add_action( 'init', 'register_acf_blocks', 5 );
 function register_acf_blocks() {
-    register_block_type( __DIR__ . './blocks/column-content-block.json' );
+    register_block_type( dirname(__FILE__) . './blocks/column-content-block.json' );
 }
--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php get_header(); ?>
 
 <div class="container mx-auto my-8">
-
-	<?php if ( have_posts() ) : ?>
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			?>
-
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-
-		<?php endwhile; ?>
-
-	<?php endif; ?>
-
+	<?php the_content(); ?>
 </div>
 
 <?php
