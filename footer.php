a7a42abf5f66d1e9533629b81d05a542e6749cf8
Todd Owen Mpeli <toddowenmpeli@rocktmail.com>
2023-09-27 13:00:57 +0100

    uncommented out the footer section.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </main>
 
-<!-- <footer class="py-6 site-footer bg-buttonblue">
+<footer class="py-6 site-footer bg-buttonblue">
     <div class="container px-8 mx-auto xl:px-0">
         
         <?php
@@ -41,7 +41,7 @@
         endif;
         ?>
     </div>
-</footer> -->
+</footer>
 
 <a class="fixed right-0 p-2 scroll-up bottom-20 bg-orange" href="#">
     <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512"><path fill="#FFF" d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/></svg>
