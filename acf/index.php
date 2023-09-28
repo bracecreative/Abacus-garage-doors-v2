194e2a0c35db69fe747a871b55392e94053f5d1c
Todd Owen Mpeli <toddowenmpeli@rocktmail.com>
2023-09-28 11:48:05 +0100

    Created the Image Row Links Component and also removed minor mistakes from process icon component. Also added a new colour to the tailwind config.

--- acf/index.php
+++ acf/index.php
@@ -10,4 +10,5 @@ function register_acf_blocks() {
     register_block_type( __DIR__ . '/blocks/title-content-image-block.json');
     register_block_type( __DIR__ . '/blocks/cta-block.json');
     register_block_type( __DIR__ . '/blocks/process-icons-block.json');
+    register_block_type( __DIR__ . '/blocks/image-row-links-block.json');
 }
