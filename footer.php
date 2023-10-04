35b90268c95ce22dc6be88c6890567d340d25abc
Gareth Gillman <gareth.gillman@brace.co.uk>
2023-10-04 09:48:49 +0100

    sidebar buttons

--- footer.php
+++ footer.php
@@ -50,35 +50,36 @@
 </a>
 
 <?php
-$side_buttons = get_field('buttons', 'option');
+$side_buttons = get_field('forms', 'option');
 if( !empty( $side_buttons ) ):
-    if( have_rows('buttons', 'option') ):
+    if( have_rows('forms', 'option') ):
 
-        while( have_rows('buttons', 'option') ): the_row();
-            $form = get_sub_field('form');
-            $form_id = $form['value'];
-            $form_title = $form['label'];
-            echo '<div id="footer-form_'.$form_title.'" class="footer-form bg-black/75 fixed h-screen left-0 top-0 w-screen z-[9999]" style="display: none">';
+        while( have_rows('forms', 'option') ): the_row();
+            $form_id = get_sub_field('form_id');
+            $form_title = get_sub_field('form_title');
+
+            echo '<div id="footer-form_'.$form_id.'" class="footer-form bg-black/75 fixed h-screen left-0 top-0 w-screen z-[9999]" style="display: none">';
                 echo '<div class="absolute bg-white h-5/6 left-2/4 overflow-y-scroll p-10 top-2/4 w-3/4 -translate-x-2/4 -translate-y-2/4">';
-                echo '<a class="close-form absolute h-[24px] inline-block right-[20px] top-[20px] w-[24px]" href="#"><svg xmlns="http://www.w3.org/2000/svg" version="1" viewBox="0 0 24 24"><path d="M13 12l5-5-1-1-5 5-5-5-1 1 5 5-5 5 1 1 5-5 5 5 1-1z"></path></svg></a>';
-                echo do_shortcode("[booking type='$form_id' nummonths=1 form_type='standard']");
+                    echo '<h2 class="font-bold mb-8 text-orange text-xl uppercase 2xl:text-2xl">'.$form_title.'</h2>';
+                    echo '<a class="close-form absolute h-[24px] inline-block right-[20px] top-[20px] w-[24px]" href="#"><svg xmlns="http://www.w3.org/2000/svg" version="1" viewBox="0 0 24 24"><path d="M13 12l5-5-1-1-5 5-5-5-1 1 5 5-5 5 1 1 5-5 5 5 1-1z"></path></svg></a>';
+                    echo do_shortcode("[booking type='$form_id' nummonths=1 form_type='standard']");
                 echo '</div>';
             echo '</div>';
         endwhile;
 
         echo '<div class="side-buttons flex-col gap-4 hidden h-full items-end justify-center relative lg:fixed lg:flex lg:right-0 lg:top-0 lg:z-[9998]">';
-            echo '<div class="inner flex gap-y-8 w-full rotate-90">';
-            while( have_rows('buttons', 'option') ): the_row();
+            echo '<div class="inner flex gap-x-8 origin-right pt-[30px] rotate-90 w-full">';
+            while( have_rows('forms', 'option') ): the_row();
 
-                $button_bg = get_sub_field('background');
-                $button_icon = get_sub_field('icon');
-                $button_text = get_sub_field('text');
+                $button_bg = get_sub_field('button_background');
+                $button_icon = get_sub_field('button_icon');
+                $button_text = get_sub_field('button_title');
                 $button_form = get_sub_field('form');
-                $button_form_id = $button_form['value'];
-                $button_form_title = $button_form['label'];
 
-                if( !empty( $button_text ) && !empty( $button_form ) ):
-                    echo '<a class="button form-btn inline-flex flex-row flex-wrap gap-4 h-auto items-center justify-center px-3 py-1 " href="#footer-form_'.$button_form_title.'" data-form-id ="'.$button_form_id.'" style="background-color:'.$button_bg.'">';
+                $button_form_id = get_sub_field('form_id');
+
+                if( !empty( $button_text ) && !empty( $button_form_id ) ):
+                    echo '<a class="button form-btn inline-flex flex-row flex-wrap gap-4 h-auto items-center justify-center px-3 py-1 " href="#footer-form_'.$button_form_id.'" data-form-id ="'.$button_form_id.'" style="background-color:'.$button_bg.'">';
                         
                         if( !empty( $button_icon ) ):
                             echo wp_get_attachment_image( $button_icon, '', 'full', array('class' => 'h-[14[x] -rotate-90 w-[14px]') );
